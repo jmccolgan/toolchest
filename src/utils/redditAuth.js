91de12bcf8d99ad25ab50d3a5972e4e2b8fe9e94
Jonathan McColgan <jonathan.mccolgan@gmail.com>
2024-11-08 11:12:45 -0600

    fixed reddit auth

--- src/utils/redditAuth.js
+++ src/utils/redditAuth.js
@@ -1,24 +1,29 @@
 const authUrl = 'https://www.reddit.com/api/v1/authorize?client_id=KYyLRwJk0iXEBAk5Ef7vCQ&response_type=token&state=12345&redirect_uri=http://localhost:5173/news&scope=read'
 
-export const redditAuthorized = () => {
-    let haveTokenCheck = JSON.parse(sessionStorage.getItem("reddit_token"))
-    console.log(haveTokenCheck)
-    if(haveTokenCheck){
-        console.log('token found')
-    }else{
-        console.log('token not found')
-        document.location = authUrl;
-        const params = new URLSearchParams(window.location.hash);
 
+export const redditAuthorized = async () => {
+    let haveTokenCheck = await JSON.parse(sessionStorage.getItem("reddit_token"))
+    const params = new URLSearchParams(window.location.hash);
+    console.log(haveTokenCheck)
+    if(haveTokenCheck != null && haveTokenCheck.accessToken != null){
+        console.log(`token found from storage ${haveTokenCheck.accessToken}`)
+        
+    } else if(params.get("#access_token")){
         let accessDetails = {
             accessToken:params.get("#access_token"),
             tokenType: params.get('token_type'),
             state: params.get("state"),
             expiresIn: params.get("expires_in"),
             scope: params.get("scope"),
+        
         }
-    
+        console.log(`token found from existing URL ${accessDetails.accessToken}`)
+
         sessionStorage.setItem("reddit_token", JSON.stringify(accessDetails));
+
+    }
+    else{
+        document.location = authUrl;
     }
 
 }
